a268226bfe00d5aa1d993f830efdd0ac160d0e64
ajadreddy007 <ajad.edavelly@testaing.com>
2023-02-03 21:28:11 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState,useEffect} from 'react'
+import Pagination from './components/Pagination';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(3);
+
+  
+  const [show,setShow] = useState(false);
+  const view = (idx) =>{
+    if(idx){
+      setShow(!show);
+    }
+  }
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(json => setData(json))
+      .catch(error => console.error(error));
+    }, []);
+    const lastPostIndex = currentPage * postsPerPage;
+    const firstPostIndex = lastPostIndex - postsPerPage;
+    const currentPosts = data.slice(firstPostIndex, lastPostIndex);
+
+    const view_arr=[]
+    for(let i=0;i<data.length;i++){
+      view_arr[i]=false;
+    }
+    //console.log(view_arr)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+      
+      {currentPosts.map(item => (
+        <div >
+          <div className='box' style={{display:'flex'}}>
+            <div >{item.company.name}</div>
+            <div >
+              <div style={{paddingBottom:'px',fontWeight:'bolder'}}>CONTACT</div>
+              <div>{item.name}</div>
+            </div>
+            <div ><div style={{paddingBottom:'px',fontWeight:'bolder'}}>CITY</div><div>{item.address.city}</div></div>
+            <div ><div style={{paddingBottom:'px',fontWeight:'bolder'}}>ZIPCODE</div><div>{item.address.zipcode}</div></div>
+            <div><button value={false} id={item.id} onClick={()=>view(item.id)} >View details</button></div> 
+          </div>
+          <div >
+          {
+            show ? <div className='box' >
+              <h3>DESCRIPTION {item.id}</h3>
+          <p>The value of the React state is toggled simply by updating with “!show“. Therefore, every time toggleShow function is called, the Boolean value of “show” state changes from true to false or vice versa.</p>
+            </div>
+            :
+            <p></p>
+          }
+
+        </div>
+          <div>
+        
+      </div>
+        </div>
+      ))}
+      
+      <Pagination
+        totalPosts={data.length}
+        postsPerPage={postsPerPage}
+        setCurrentPage={setCurrentPage}
+        currentPage={currentPage}
+    />
     </div>
   );
 }
